6c61ecd747c86d4a1acb152051c75b4715961fe2
BasantMahmoud <elsayedbasant21@gmail.com>
2022-06-11 15:45:39 +0200

    Add all my files

---
+++ script.js
@@ -0,0 +1,45 @@
+/* 
+<div id="accordion" style="margin: 0; top: 0; " >
+    <!-- 1 -->
+    <div class="card ">
+      <div class="card-header">
+        <a class="btn dropdown "  data-bs-toggle="collapse" href="#collapseOne" >
+          python programming language <i class="far fa-angle-down"></i>
+        </a>
+      </div>
+      <div id="collapseOne" class="collapse show" data-bs-parent="#accordion">
+        <div class="card-body">
+          Python is powerful... and fast, plays well with others,runs everywhere, is friendly & easy to learn, is Open.
+        </div>
+      </div>
+      <!-- 2 -->
+      <div class="card">
+        <div class="card-header">
+          <a class="btn dropdown "  data-bs-toggle="collapse" href="#collapseOne" >
+            java programming language <i class="far fa-angle-down"></i>
+          </a>
+        </div>
+        <div id="collapseOne" class="collapse show" data-bs-parent="#accordion">
+          <div class="card-body">
+            Java is a powerful general-purpose programming language. It is used to develop desktop and mobile applications, big data processing, embedded systems, and so on. According to Oracle, the company that owns Java, Java runs on 3 billion devices worldwide, which makes Java one of the most popular programming languages.
+          </div>
+        </div>
+         <!-- 3 -->
+      <div class="card">
+        <div class="card-header">
+          <a class="btn dropdown "  data-bs-toggle="collapse" href="#collapseOne" >
+            javascript programming language <i class="far fa-angle-down"></i>
+          </a>
+        </div>
+        <div id="collapseOne" class="collapse show" data-bs-parent="#accordion">
+          <div class="card-body">
+            JavaScript is a scripting or programming language that allows you to implement complex features on web pages.
+          </div>
+        </div>
+    </div>
+</section>   <!-- end main section -->
+
+<i class="far fa-angle-down"></i>
+
+
+*/
